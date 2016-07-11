987eb72b10607c70d7a1ed2f8373467173262b12
Joris van Lieshout <jvanlieshout@schubergphilis.com>
2016-07-11 11:16:24 -0500

    save my work throughout the morning

--- ark/spec/spec_helper.rb
+++ ark/spec/spec_helper.rb
@@ -5,7 +5,41 @@ at_exit { ChefSpec::Coverage.report! }
 
 RSpec.configure do |config|
   config.color = true
+  config.log_level = :info
+  config.alias_example_group_to :describe_recipe, type: :recipe
+end
+
+shared_context 'converge recipe', type: :recipe do
+  let(:chef_run) do
+    runner = ChefSpec::SoloRunner.new(node_attr)
+    runner.converge(described_recipe)
+  end
+
+  let(:node_attr) do
+    {}
+  end
+
+  let(:node) do
+    chef_run.node
+  end
+
+  def attribute(name)
+    node[described_cookbook][name]
+  end
+
+  shared_examples 'installs packages' do
+    it 'installs necessary packages' do
+      installed_packages.each { |name| expect(chef_run).to install_package(name) }
+    end
+  end
+
+  shared_examples 'does not install packages' do
+    it 'does not install packages' do
+      packages_not_installed.each { |name| expect(chef_run).not_to install_package(name) }
+    end
+  end
 end
 
 # puts $LOAD_PATH
 # ... define test helpers and content in this file
+
--- ark/spec/unit/recipes/default_spec.rb
+++ ark/spec/unit/recipes/default_spec.rb
@@ -1,99 +1,68 @@
 require 'spec_helper'
 
-describe 'ark::default' do
+describe_recipe 'ark::default' do
   context 'when no attributes are specified, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new
-      runner.converge(described_recipe)
+
+    let(:installed_packages) do
+      %w( libtool autoconf unzip rsync make gcc autogen )
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
-    end
+    it_behaves_like 'installs packages'
 
-    it "does not install the gcc-c++ package" do
-      expect(chef_run).not_to install_package("gcc-c++")
-    end
+    let(:packages_not_installed) do
+      %w( gcc-c++ seven_zip )
+    end 
 
-    it "does not include the seven_zip recipe" do
-      expect(chef_run).not_to include_recipe("seven_zip")
-    end
+    it_behaves_like 'does not install packages'
 
     it "apache mirror" do
-      attribute = chef_run.node['ark']['apache_mirror']
-      expect(attribute).to eq "http://apache.mirrors.tds.net"
+      expect(attribute('apache_mirror')).to eq "http://apache.mirrors.tds.net"
     end
 
     it "prefix root" do
-      attribute = chef_run.node['ark']['prefix_root']
-      expect(attribute).to eq "/usr/local"
+      expect(attribute('prefix_root')).to eq "/usr/local"
     end
 
     it "prefix bin" do
-      attribute = chef_run.node['ark']['prefix_bin']
-      expect(attribute).to eq "/usr/local/bin"
+      expect(attribute('prefix_bin')).to eq "/usr/local/bin"
     end
 
     it "prefix home" do
-      attribute = chef_run.node['ark']['prefix_home']
-      expect(attribute).to eq "/usr/local"
+      expect(attribute('prefix_home')).to eq "/usr/local"
     end
 
     it "tar binary" do
-      attribute = chef_run.node['ark']['tar']
-      expect(attribute).to eq "/bin/tar"
+      expect(attribute('tar')).to eq "/bin/tar"
     end
   end
 
   context 'when no attributes are specified, on CentOS' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '6.7')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'centos', version: '6.7' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('xz-lzma-compat')
-      expect(chef_run).to install_package('bzip2')
-      expect(chef_run).to install_package('tar')
+    let(:installed_packages) do
+      %w( libtool autoconf unzip rsync make gcc xz-lzma-compat bzip2 tar )
     end
+
+    it_behaves_like 'installs packages'
   end
 
   context 'when no attributes are specified, on Debian' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', platform_family: 'debian', version: '14.04')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'ubuntu', platform_family: 'debian', version: '14.04' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
-      expect(chef_run).to install_package('shtool')
-      expect(chef_run).to install_package('pkg-config')
+    let(:installed_packages) do
+      %w( libtool autoconf unzip rsync make gcc autogen shtool pkg-config )
     end
 
+    it_behaves_like 'install package'
   end
 
   context 'when no attributes are specified, on FreeBSD' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'freebsd', version: '10.2')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'freebsd', platform_family: 'freebsd', version: '10.2' }
     end
 
     it 'installs necessary packages' do
@@ -114,9 +83,8 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on Mac OSX' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.11.1')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'mac_os_x', platform_family: 'mac_os_x', version: '10.11.1' }
     end
 
     it 'does not install packages' do
@@ -135,9 +103,8 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on RHEL' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'redhat', platform_family: 'rhel', version: '6.5')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'redhat', platform_family: 'rhel', version: '6.5' }
     end
 
     it 'installs necessary packages' do
@@ -154,9 +121,8 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on SmartOS' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'smartos', version: '5.11')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'smartos', version: '5.11' }
     end
 
     it 'installs necessary packages' do
@@ -177,9 +143,8 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on Windows' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2')
-      runner.converge(described_recipe)
+    let(:node_attr) do
+      { platform: 'windows', version: '2012R2' }
     end
 
     it 'does not installs packages' do
